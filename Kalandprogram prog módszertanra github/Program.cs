6b3333d18c343e3be61b962b7e425f8ebeecff32
Th3Trig3rFac3 <66511756+Th3Trig3rFac3@users.noreply.github.com>
2020-10-29 12:31:40 +0100

    Add files via upload

---
+++ Kalandprogram prog módszertanra github/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kalandprogram_prog_módszertanra
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Opening screen
+            Console.WriteLine("Üdvözöllek ebben a szöveges kalandjátékban, érezd jól magad!" +
+                "\nEmlékezz, hogy minden döntésednek hatása van a jövőre." +
+                "\nA múltbeli döntéseid lehetőségeket nyithatnak, de el is vehetik azokat. Légy óvatos és jó kaladozást" +
+                "\nv:0.1 Ez a verzió még nem tartalmazza a progress mentését!");
+
+            do
+            {
+                Console.Write("Nyomj egy entert a kezdéshez!");
+            } while (Console.ReadKey().Key != ConsoleKey.Enter);
+            Console.Clear();
+
+            //Rules
+
+
+            //Gameplay
+
+
+        }
+    }
+}
